314d1e9a1670c74f3df02ca63957d972cb0d4b28
Marco H <153363103+mhoek2@users.noreply.github.com>
2025-03-28 14:01:30 +0100

    Add PHPStan and initial config for static analysis

---
+++ phpstan-bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+// Load CodeIgniter helper functions
+require_once 'vendor/codeigniter4/framework/system/Helpers/url_helper.php';
+require_once 'vendor/codeigniter4/framework/system/Helpers/text_helper.php';
+
+// Load framework bootstrap (if not already loaded)
+if (file_exists('vendor/codeigniter4/framework/system/Test/bootstrap.php')) {
+    require_once 'vendor/codeigniter4/framework/system/Test/bootstrap.php';
+}
