dc7326d318d7b6f9684b8c0ce0583209fc1c2d35
mhoek2 <153363103+mhoek2@users.noreply.github.com>
2025-04-16 10:06:15 +0200

    Quick parsing function to visualize user_meta better

--- app/Views/front/dashboard_modules/example.php
+++ app/Views/front/dashboard_modules/example.php
@@ -1,1 +1,35 @@
+<?php
+function parse_entry_meta( $data ){
+	if ( strlen($data) < 1 )
+		return;
+
+	$entries = json_decode($data, true);
+
+	if ( !is_array($entries) || empty($entries) )
+		return;
+	
+	foreach( $entries as $entry )
+	{
+		echo "<b>" . $entry['entry_name'] . "</b><br>";
+
+		// mcq?
+		if ( !is_null($entry['property_id']) && is_array($entry['value']) )
+		{
+			foreach( $entry['value'] as $value )
+			{
+				echo $value . "<br>";
+			}
+		}
+		else {
+			echo $entry['value'] . "<br>";
+		}
+
+		echo "<br><br>";
+	}
+}
+
+parse_entry_meta( $meta['value'] );
+?>
+
+<b>data:</b><br>
 <?= $meta['value'] ?>
